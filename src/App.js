939147e25e2295b45ef29befa7d349568db3e7f6
anuj jain <anujjain@externlabs.com>
2019-11-27 17:58:30 +0530

    json to pdf convertor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {Button} from 'antd'
+import jsPDF from 'jspdf'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component {
+  jsPdfGenerator = () => {
+
+    // Example From https://parall.ax/products/jspdf
+    var doc = new jsPDF('p', 'pt');
+
+    doc.text(20, 20, 'This is the default font.')
+
+    doc.setFont('courier')
+    doc.setFontType('normal')
+    doc.text(20, 30, 'This is courier normal.')
+
+    doc.setFont('times')
+    doc.setFontType('italic')
+    doc.text(20, 40, 'This is times italic.')
+
+    doc.setFont('helvetica')
+    doc.setFontType('bold')
+    doc.text(20, 50, 'This is helvetica bold.')
+
+    doc.setFont('courier')
+    doc.setFontType('bolditalic')
+    doc.text(20, 60, 'This is courier bolditalic.')
+
+    // Save the Data
+    doc.save('Generated'.pdf)
+  }
+  render() {
+    return (
+      <Button onClick={this.jsPdfGenerator} type="primary"> Generate PDF </Button> 
+
+    );
+
+  }
+}
 export default App;
